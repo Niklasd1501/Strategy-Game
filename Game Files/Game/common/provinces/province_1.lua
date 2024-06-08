2334e17824d4afa270c5e760d4c1bc1811a449e6
Niklasd1501 <niklas_droege@outlook.de>
2024-06-08 14:57:52 +0200

    Add project files.

---
+++ Game Files/Game/common/provinces/province_1.lua
@@ -0,0 +1,23 @@
+province={
+	id=1,
+	owner="PRE",
+	controller="PRE",
+	resources={
+		{
+			name="Wood",
+			amount="45"
+		},
+		{
+			name="Coal",
+			amount="15"
+		},
+		{
+			name="Iron",
+			amount="60"
+		},
+		{
+			name = "Wheat",
+			amount = "145"
+		}
+	}
+}
---
+++ Game Files/Game/common/states/state_1.lua
@@ -0,0 +1,10 @@
+state={
+	id=1,
+	name="Test State",
+	provinces={
+		1,
+		24,
+		12,
+		10
+	}
+}
